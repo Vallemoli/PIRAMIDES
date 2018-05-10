8cb06369865bbc0f5b59e8798008b160b0c2d73f
josem <josem@LAPTOP-DSNU6TIN>
2018-05-10 10:47:49 +0200

    Primer commit para añadir el proyecto al repositorio

---
+++ Piramides/src/piramides1.java
@@ -0,0 +1,55 @@
+
+public class piramides1 {
+
+	public static void main(String[] args) {
+
+		int base= 9;
+		
+		//Calcular el nº de filas
+		int filas= (base+1)/2;
+		
+		/*
+		 * Para cada fila desde 0 hasta filas -1
+		 * 
+		 * 1. Calcular los blancos + imprimirlos
+		 * 2. Calcular los asteriscos + imprimirlos
+		 * 
+		 */
+		
+		for (int i = 0; i < filas; i++) {
+			
+			// i es el número de la fila
+			/*
+			 * Hacemos una tabla con i y el numero de blancos
+			 * correspondientes a cada i
+			 * Vemos que la sumas es constante, asi que podemos formular
+			 * 
+			 * i+blancos= filas-1
+			 * 
+			 * asi que si se despeja blancos
+			 * 
+			 * blancos= filas-1-i
+			 */
+			
+			//calcular e imprimir blancos
+			for (int j=0; j<filas-1-i; j++)
+				System.out.print(' ');
+			
+			/*
+			 * para calcular los asteriscos realizamos un razonamiento
+			 * análogos a los blancos
+			 */
+			
+			for(int j=0; j<2*i+1; j++)
+				System.out.print('*');
+			
+			//los blancos de final no se hace falta calcularlos
+			
+			//imprimir salto de línea
+			System.out.print('\n');
+			
+		}//for
+		
+	}//main
+
+}//class
